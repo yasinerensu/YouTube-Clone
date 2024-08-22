9d8e5b4cb8c6c5db5cf8aa5fa25caa8cca39547f
Erensu Oba <yasinerensu@gmail.com>
2024-08-22 22:33:09 +0300

    modified:   index.html, script.js, styles.css

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const menuButton = document.querySelectorAll(".menu-button");
+const screenOverlay = document.querySelector(".screen-overlay");
+const themeButton = document.querySelector(".theme-button i");
+
+
+
+if (localStorage.getItem("darkMode") === "enabled") {
+  document.body.classList.add("dark-mode");
+  themeButton.classList.replace("uil-moon", "uil-sun");
+}else{
+  themeButton.classList.replace("uil-sun", "uil-moon");
+}
+
+themeButton.addEventListener("click", () => {
+  const isDarkMode = document.body.classList.toggle("dark-mode");
+  localStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
+  themeButton.classList.toggle("uil-sun", isDarkMode);
+  themeButton.classList.toggle("uil-moon", !isDarkMode);
+});
 
 menuButton.forEach(button => {
   button.addEventListener("click", () => {
     document.body.classList.toggle("sidebar-hidden");
   });
 });
+
+screenOverlay.addEventListener("clik", () => {
+  document.body.classList.toggle("sidebar-hidden");
+});
  
+if(window.innerWidth >= 768){
+  document.body.classList.remove("sidebar-hidden");
+}
